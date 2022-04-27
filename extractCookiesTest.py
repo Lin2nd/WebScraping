491241f9ff8ac5d636636bb5d22bf20c4615258e
Lin2nd <lin.tian@studenti.pilito.it>
2022-04-27 08:12:50 -0700

    add extractCookiesTest

---
+++ extractCookiesTest.py
@@ -0,0 +1,48 @@
+import urllib
+import http.cookiejar
+import requests
+from selenium import webdriver
+from time import sleep
+
+
+URL = "https://jcr.clarivate.com/jcr/home"
+
+# Not working. Empty list.
+def extract_cookies1():
+    cookie_jar = http.cookiejar.CookieJar()
+    url_opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie_jar))
+    url_opener.open(URL)
+    print(url_opener)
+    print(cookie_jar)
+    for cookie in cookie_jar:
+        print(cookie.name, ": ", cookie.value)
+
+# Not working. Empty list
+def extract_cookies2():
+    response = requests.get(URL)
+    print(response)
+    print(response.cookies)
+    for cookie in response.cookies:
+        print(cookie.name, ": ", cookie.value)
+
+# Not working. Empty list
+def extract_cookies3():
+    driver = webdriver.Chrome()
+    driver.get(URL)
+    sleep(3)
+    cookies = driver.get_cookies()
+    print(cookies)
+    for cookie in cookies:
+        pass
+
+# Weird but working
+def extract_cookies4():
+    driver = webdriver.Chrome()
+    driver.get(URL)
+    sleep(3)
+    cookie = driver.execute_script("return document.cookie.match(\"PSSID=(.*?);\")")
+    print(cookie[1])
+
+
+if __name__ == "__main__":
+    extract_cookies4()
